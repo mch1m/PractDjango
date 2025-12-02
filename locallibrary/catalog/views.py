08b246d54c418c07ef9de7347c925db9536dc167
mch1m <chimismortyiool@gmail.com>
2025-12-03 01:20:32 +0700

    adding pages, authorization

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
     """
@@ -11,12 +12,31 @@ def index(request):
     # Доступные книги (статус = 'a')
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.count()  # Метод 'all()' применён по умолчанию.
-
+    num_genres=Genre.objects.count()
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+        context={'num_books':num_books,'num_instances':num_instances,
+                 'num_instances_available':num_instances_available,
+                 'num_authors':num_authors, 'num_genres':num_genres, 'num_visits':num_visits},
     )
 # Create your views here.
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 1
+
+
+class BookDetailView(generic.DetailView):
+        model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 1
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -21,9 +21,22 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+    path('admin/', admin.site.urls)
 ]
+# добавляем URL для приложения catalog
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+
+]
+
+# перенаправление с корневого URL на catalog/
+urlpatterns += [
+path('', RedirectView.as_view(url='/catalog/', permanent=True))
+]
+
+# добвляем обработку статических файлов(только для разработки)
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
